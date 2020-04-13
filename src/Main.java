ff5c4ee6dc3d4da33f5870cfdd0dc599944ef506
Den <plizzz.healme@gmail.com>
2020-04-13 12:54:58 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Beep a1 = new Car();
-        Beep a2 = new Jeep();
-
-        a1.beep();
-        a2.beep();
+        Beep b = new Car();
+        b.beep();
+        b = new Jeep();
+        b.beep();
     }
 }
